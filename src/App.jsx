1c23d0eb5bc34eb908ecf6fbb71944bde0c4f501
enimocar <enimocar@gmail.com>
2023-06-10 21:09:21 +0200

    Añadiendo un input para las cantidades

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ function App() {
     // Es solo visualización
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
+
+      <input type="range" className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"/>
     </div>
   )
 }
